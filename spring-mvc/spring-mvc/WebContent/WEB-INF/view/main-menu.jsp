8ecbfe7ad0ad27ba73cec44af7406595bafd0675
VeeShostak <VeeShostak@users.noreply.github.com>
2017-09-28 18:01:23 -0400

    spring MVC, adding data to Model

--- spring-mvc/spring-mvc/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc/spring-mvc/WebContent/WEB-INF/view/main-menu.jsp
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
+<!-- 
+<head>
 
+ <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/my-test.css">
+
+ <script src="${pageContext.request.contextPath}/resources/js/simple-test.js"></script>
+
+</head>
+ -->
+ 
 <body>
+
+<!-- <img src="${pageContext.request.contextPath}/resources/images/spring-logo.png">
+-->
 
 <h1>Spring MVC - Home Page</h1>
 <hr>
 
 <p><a href="showForm">Hello World form</a></p>
+
+
 
 </body>
 </html>
--- spring-mvc/spring-mvc/src/com/github/veeshostak/spring/mvc/HelloWorldController.java
+++ spring-mvc/spring-mvc/src/com/github/veeshostak/spring/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.github.veeshostak.spring.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,4 +20,23 @@ public class HelloWorldController {
 	public String processForm() {
 		return "helloworld";
 	}
+	
+	// read form data and
+	// add data to the modal
+	@RequestMapping("/processFormVersionTwo")
+	public String makeTextShout(HttpServletRequest request, Model model) {
+		// read the request parameter from the html form
+		String theName = request.getParameter("studentName");
+		
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create message and add to the modal
+		String result = " the name is " + theName;
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	
 }
