85f7efcfb576c9bf40ed3d24b6755dec5f29a0a5
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-02 16:54:24 -0400

    Hibernate One to Many Bi-directional

---
+++ hibernate/hb-03-one-to-many/src/com/github/veeshostak/demo/CreateInstructorDemo.java
@@ -0,0 +1,65 @@
+package com.github.veeshostak.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.github.veeshostak.hibernate.entity.Course;
+import com.github.veeshostak.hibernate.entity.Instructor;
+import com.github.veeshostak.hibernate.entity.InstructorDetail;
+
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// create the objects			
+			Instructor tempInstructor = 
+					new Instructor("Sandy", "TestLast", "susan@yahoo.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.youtube.com",
+							"Guitar");		
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// also saves the details object due to cascading
+			
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);					
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+		}
+		finally {
+			
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
